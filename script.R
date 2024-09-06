046991ccf31002c0060738d88c63a3de272345d7
Mathieuforestdata <penet.mathieu@free.fr>
2024-09-06 21:29:56 +0200

    Mathieu : Update accroissement en g/ha

--- script.R
+++ script.R
@@ -14,6 +14,7 @@ install.packages("ggplot2")
 install.packages("dplyr")
 install.packages("happifn")
 install.packages("tidyr")
+install.packages("tmap")
 
 # Installation du dossier de travail ----
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -31,6 +32,7 @@ library(sf)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(tmap);ttm
 
 # Chargement des fonction de récupération des données IFN ----
 #get_ifn_all()
@@ -72,7 +74,7 @@ st_crs(shp_etude)  # Test projetction zone d'étude
 # Alors on ajoute un buffer au shp importé
 
 # Définition de la zone tampon
-largeur_tampon <- 2000  # Ajustez cette valeur selon vos besoins
+largeur_tampon <- 50000  # Ajustez cette valeur selon vos besoins
 
 # Création de la zone tampon autour du shapefile
 zone_tampon <- st_buffer(shp_etude, dist = largeur_tampon)
@@ -102,9 +104,20 @@ idp_placette_tampon <- placette_tampon$IDP
 
 arbre_zone_etude <- arbre[arbre$IDP %in% idp_placette_tampon, ]
 arbre_zone_etude$Essence <- NA
+# Fabrication d'un numéro unique
 arbre_zone_etude$num_unique <- paste(arbre_zone_etude$IDP,
                                      arbre_zone_etude$A,
                                      sep = ".")
+
+# Créer une nouvelle colonne qui stocke toutes les années de campagne pour chaque IDP
+arbre_zone_etude <- arbre_zone_etude %>%
+  group_by(num_unique) %>%
+  mutate(
+    annee_mesure = paste(unique(CAMPAGNE), collapse = ", "),# Créer une chaîne de caractères avec les années uniques
+    circ_mesure = paste(na.omit(C13), collapse = ", ")  # Créer une liste avec les mesures de circonférecnes
+  ) %>%
+  ungroup()
+
 # Les sections vides dans ESPAR sont remplacé par NA
 arbre_zone_etude$ESPAR[arbre_zone_etude$ESPAR == "" | arbre_zone_etude$ESPAR == " "] <- NA
 
@@ -135,7 +148,7 @@ for (i in 1:nrow(arbre_zone_etude_cor)) {
 
 # On sélectionne uniquement quelques colonnes utile
 arbre_zone_etude_cor <- arbre_zone_etude_cor %>%
-  select(CAMPAGNE, num_unique, Essence, C13,C0, HTOT, HDEC, V, W, IR5, IR1) %>%
+  select(CAMPAGNE, num_unique, Essence, C13,C0, HTOT, HDEC, V, W, IR5, IR1, annee_mesure, circ_mesure) %>%
   
   pivot_wider(names_from = CAMPAGNE,   # Créer des colonnes pour chaque année
               values_from = C13) %>%  # Les valeurs à placer dans ces colonnes sont les circonférences mesurées
@@ -145,37 +158,125 @@ arbre_zone_etude_cor <- arbre_zone_etude_cor %>%
   
 # On cherche la circonférence max dans la liste
 arbre_zone_etude_cor <- arbre_zone_etude_cor %>%
-  # On vérifie que toutes les colonnes de date de mesure soit numérique
-  mutate(across(starts_with("20"), as.numeric)) %>%
-  rowwise() %>%
-  # Dans toutes les colonnes date, on cherche la valeur max de circonférence
-  mutate(circonference_max = max(c_across(starts_with("20")), na.rm = TRUE),
-         circonference_min = min(c_across(starts_with("20")), na.rm = TRUE),
-         acc_moy = circonference_max - circonference_min) %>%
-  ungroup() %>%
+  group_by(num_unique) %>%
+  # Calculer l'accroissement annuel
+  mutate(
+    # Séparer la chaîne de caractères en vecteur d'années pour chaque ligne
+    annee_vector = list(as.numeric(strsplit(annee_mesure, ",\\s*")[[1]])),
+    circ_vector = list(as.numeric(strsplit(circ_mesure, ",\\s*")[[1]])),
+    
+    # Extraire l'année maximale et minimale
+    annee_max = max(annee_vector[[1]], na.rm = TRUE),  # L'année de la circonférence max
+    annee_min = min(annee_vector[[1]], na.rm = TRUE),  # L'année de la circonférence min
+    
+    # Extraire la circonférence maximale et minimale
+    circonference_max = max(circ_vector[[1]], na.rm = TRUE),  # La circonférence max
+    circonference_min = min(circ_vector[[1]], na.rm = TRUE))%>%  # La circonférence min
+    
+    mutate(cat_diam = case_when(
+      circonference_max >= 0.235 & circonference_max < 0.705 ~ "PB",   # Petite Bois (PB)
+      circonference_max >= 0.705 & circonference_max < 1.175 ~ "BM",  # Bois Moyen (BM)
+      circonference_max >= 1.175 ~ "GB",  # Gros Bois (GB))
+      TRUE ~ NA_character_ ))%>%
   
+    mutate(w = case_when(
+      cat_diam == "PB" ~ 88.4,
+      cat_diam == "BM" ~ 39.3,
+      cat_diam == "GB" ~ 14.1,
+      TRUE ~ NA_real_)) %>%
   
+    mutate(
+      # On calcul le diamètre de l'arbre
+      diam = round((circonference_max / pi)/0.01,1),
+      # On calcul sa classe de diam
+      clas_diam = round(diam / 5) * 5
+    ) %>%
   
-  
-  
-  # On calcul le diamètre de la circonférence max
-  mutate(diametre = circonference_max / pi) %>%  # Diamètre exacte
-  mutate(diametre = round(diametre / 0.001) * 0.001*100) %>%
-  mutate(class_diametre = round(diametre /5) * 5) %>% # On arrondi
-  
-  # On définis les catégories de diamètre
-  mutate(cat_diam = case_when(
-    diametre >= 7.5 & diametre < 22.5 ~ "PB",   # Petite Bois (PB)
-    diametre >= 22.5 & diametre < 47.5 ~ "BM",  # Bois Moyen (BM)
-    diametre >= 47.5 & diametre < 67.5 ~ "GB",  # Gros Bois (GB)
-    diametre >= 67.5 ~ "TGB",                  # Très Gros Bois (TGB)
-    TRUE ~ NA_character_                       # Gérer les cas manquants ou en dehors des intervalles
-  ))
+  mutate(
+    # Calcul accroissement en G/ha/an
+    acc_g_ha = round((((circonference_max^2) / (4 * pi)) * w ) / (annee_max - annee_min),2)
+  )
+
+
+# Calculer les moyennes d'accroissement par essence et catégorie de diamètre
+# Puis les affciher dans un tableau récapitulatif
+table_recap <- arbre_zone_etude_cor %>%
+  filter(is.finite(acc_g_ha)) %>%  # Exclure les valeurs infinies
+  group_by(Essence, cat_diam) %>%
+  summarise(moyenne_accroissement = mean(acc_g_ha, na.rm = TRUE), .groups = 'drop') %>%
+  pivot_wider(names_from = cat_diam, values_from = moyenne_accroissement, values_fill = list(moyenne_accroissement = NA)) %>%
+  arrange(Essence)
+
+# Afficher le tableau récapitulatif
+print(table_recap)
 
 
 
 
 
+
+
+
+
+
+
+
+# Modif roman 06/09, fonction sylvoécorégion ----
+  check_happifndata()
+  library(happifndata)  
+  placette <- get_ifn("placette")  # Charger l'objet 'placette' à partir d'un fichier ou d'une source
+  data("ser")
+  
+  get_placette <- function(sylvoecoregion) {
+    # Charger les données ser et des placettes
+    data("ser")           # Charger l'objet 'ser'
+    
+    # Filtrer le polygone correspondant au nom donné
+    polygone <- ser %>% filter(ser[[2]] == sylvoecoregion)
+    
+    if (nrow(polygone) == 0) {
+      stop("Le nom du polygone n'existe pas dans la table.")
+    }
+    
+    # S'assurer que les systèmes de coordonnées sont identiques
+    if (st_crs(placette) != st_crs(polygone)) {
+      placette <- st_transform(placette, st_crs(polygone))
+    }
+    
+    # Utiliser une approche vectorisée pour la vérification
+    within_list <- st_within(placette, polygone)
+    
+    # Filtrer les placettes qui sont dans le polygone
+    placettes_dans_polygone <- placette[lengths(within_list) > 0, ]
+    
+    return(placettes_dans_polygone)
+  }
+  
+  test <- get_placette(sylvoecoregion = "Ardenne primaire")
+  View(test)
+  #fontion pour avoir tout les arbres d'une sylvoecoregion
+  
+  get_arbre_parcelle<- function(sylvoecoregion){
+    placette_etude <- get_placette(sylvoecoregion)
+    for (i in 1:nrow(placette_etude)){
+      for (j in 1:nrow(arbre)){
+        if (arbre[j, "IDP"] == placette_etude[i, "IDP"]) {
+          arbre_etudie <- add_row(arbre_etudie, arbre[j, ])
+        }
+        
+      }
+    }
+    return(arbre_etudie)
+  }
+  
+  
+  test <- get_arbre_parcelle(sylvoecoregion = "Ardenne primaire")
+  
+  print(test)
+  
+  get_placette("Ardenne primaire")
+  
+qtm(test)
 
 # Descriptif de la donnée brut ARBRE ----
 
